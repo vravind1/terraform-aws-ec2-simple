98e5ece56917a19f5f420e07e67e7378f9edfbdf
Vishnu Ravindra <vishnu.ravindra@hashicorp.com>
2023-03-30 19:48:43 -0400

    Create main.tf
    main.tf added

---
+++ main.tf
@@ -0,0 +1,37 @@
+# Define the provider
+provider "aws" {
+  region = var.aws_region
+}
+
+# Define the EC2 instance
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = [var.security_group_id]
+
+  tags = {
+    Name = var.instance_name
+  }
+
+  # Define the connection for SSH access
+  connection {
+    type        = "ssh"
+    user        = var.ssh_user
+    private_key = file(var.ssh_private_key_path)
+    host        = self.public_ip
+  }
+
+  # Define the provisioner for installing software
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y nginx",
+    ]
+  }
+}
+
+# Define the output for accessing the instance
+output "public_ip" {
+  value = aws_instance.example.public_ip
+}
